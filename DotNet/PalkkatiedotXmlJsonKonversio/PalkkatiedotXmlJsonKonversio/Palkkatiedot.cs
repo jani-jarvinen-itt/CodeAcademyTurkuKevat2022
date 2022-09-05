9b60c27fa247701ce88b7a8d61c113690ca0e264
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-09-05 13:31:57 +0300

    Lisätty XML-käsittelyesimerkki sekä muunnos JSON-muotoon. Palkkatiedot sekä valuuttakurssit.

---
+++ DotNet/PalkkatiedotXmlJsonKonversio/PalkkatiedotXmlJsonKonversio/Palkkatiedot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalkkatiedotXmlJsonKonversio
+{
+    public class Palkkatiedot
+    {
+        public string personName { get; set; }
+        public Kuukausipalkka salary { get; set; }
+        public string hiredate { get; set; }
+    }
+
+    public class Kuukausipalkka
+    {
+        public double monthly { get; set; }
+    }
+}
---
+++ DotNet/PalkkatiedotXmlJsonKonversio/PalkkatiedotXmlJsonKonversio/Program.cs
@@ -0,0 +1,59 @@
+﻿using PalkkatiedotXmlJsonKonversio;
+using System.Text.Json;
+using System.Xml.Linq;
+
+// vaihe 1: USD-valuuttakurssin luku XML-tiedostosta
+string tiedostoNimi = @"C:\Academy\Turku\DotNet\PalkkatiedotXmlJsonKonversio\PalkkatiedotXmlJsonKonversio\Aineisto\Valuuttakurssi.xml";
+XDocument xmlDokumentti = XDocument.Load(tiedostoNimi);
+
+XNamespace gesmes = "http://www.gesmes.org/xml/2002-08-01";
+XNamespace ns = "http://www.ecb.int/vocabulary/2002-08-01/eurofxref";
+var cubes = xmlDokumentti.Descendants(ns + "Cube")
+               .Where(x => x.Attribute("currency") != null)
+               .Select(x => new
+               {
+                   Currency = (string)x.Attribute("currency"),
+                   Rate = (double)x.Attribute("rate")
+               });
+
+double usdKurssi = 0.0;
+foreach (var result in cubes)
+{
+    // Console.WriteLine($"{result.Currency}: {result.Rate}");
+    if (result.Currency == "USD")
+    {
+        usdKurssi = result.Rate;
+        break;
+    }
+}
+Console.WriteLine(usdKurssi);
+
+// vaihe 2: palkkatietojen luku XML-tiedostosta
+tiedostoNimi = @"C:\Academy\Turku\DotNet\PalkkatiedotXmlJsonKonversio\PalkkatiedotXmlJsonKonversio\Aineisto\Palkanlaskenta.xml";
+xmlDokumentti = XDocument.Load(tiedostoNimi);
+var työntekijät = xmlDokumentti.Descendants("palkka")
+               .Select(x => new
+               {
+                   PersonName = (string)x.Descendants("nimi").First(),
+                   Salary = (double)x.Descendants("kuukausittain").First(),
+                   HireDate = (string)x.Descendants("työsuhdealkoi").FirstOrDefault()
+               });
+
+// muunnetaan luettu XML-data olioksi
+List<Palkkatiedot> tiedot = new();
+foreach (var työntekijä in työntekijät)
+{
+    tiedot.Add(new Palkkatiedot()
+    {
+        personName = työntekijä.PersonName,
+        hiredate = työntekijä.HireDate,
+        salary = new Kuukausipalkka()
+        {
+            monthly = työntekijä.Salary * usdKurssi
+        }
+    });
+}
+
+// vaihe 3: palkkatietojen kirjoitus JSON-muotoon
+string json = JsonSerializer.Serialize(tiedot);
+Console.WriteLine(json);
