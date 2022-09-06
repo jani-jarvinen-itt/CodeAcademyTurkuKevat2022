e4d2410f69644ffdec893cb32202b3e8908ee14f
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-09-06 11:35:33 +0300

    Lisätty ASP.NET Web API -esimerkki omalla kontrollerilla ja Swagger-tuella.

---
+++ DotNet/WebApiDemo/WebApiDemo/Controllers/OmaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("xyz")]
+    [ApiController]
+    public class OmaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("aaa")]
+        public string Moi()
+        {
+            return "Moikka!";
+        }
+
+        [HttpPut]
+        [Route("bbb")]
+        public string Terve()
+        {
+            return "Heissan!";
+        }
+    }
+}
