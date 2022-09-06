8f94db8a7f6b86a8ce406421b45c4ae5a0411d73
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-09-06 13:22:33 +0300

    Lisätty uusi kontrolleri jossa testataan eri tietotyyppien JSON-serialisointia.

---
+++ DotNet/WebApiDemo/WebApiDemo/Controllers/TestausController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestausController : ControllerBase
+    {
+        [HttpGet]
+        [Route("kellonaika")]
+        public DateTime Kellonaika()
+        {
+            return DateTime.Now;
+        }
+
+        [HttpGet]
+        [Route("luvut")]
+        public int[] Luvut()
+        {
+            int[] luvut = { 1, 3, 5, 7, 9 };
+            return luvut;
+        }
+
+        [HttpGet]
+        [Route("anonyymi")]
+        public object AnonyymiOlio()
+        {
+            return new
+            {
+                Ominaisuus1 = 9876,
+                Ominaisuus2 = "ABCD"
+            };
+        }
+    }
+}
