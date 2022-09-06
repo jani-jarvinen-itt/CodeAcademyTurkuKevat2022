6251eceb479b498780656eda0376349fa16fdb12
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-09-06 15:07:02 +0300

    Lisätty asiakas-kontrolleri joka hakee kaikki asiakkaat Northwind-tietokannasta.

---
+++ DotNet/WebApiDemo/WebApiDemo/Controllers/AsiakasController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiDemo.Entities;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AsiakasController : ControllerBase
+    {
+        public List<Customer> KaikkiAsikkaat()
+        {
+            NorthwindContext context = new();
+            List<Customer> asiakkaat = context.Customers.ToList();
+
+            return asiakkaat;
+        }
+    }
+}
