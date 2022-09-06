01555c1fb91cf531805fac28b7d8d0def365a909
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-09-06 14:04:09 +0300

    Lisätty esimerkki JSON-datan sisäänlukuun.

--- DotNet/WebApiDemo/WebApiDemo/Controllers/TestausController.cs
+++ DotNet/WebApiDemo/WebApiDemo/Controllers/TestausController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApiDemo.Mallit;
 
 namespace WebApiDemo.Controllers
 {
@@ -31,6 +32,23 @@ namespace WebApiDemo.Controllers
                 Ominaisuus1 = 9876,
                 Ominaisuus2 = "ABCD"
             };
+        }
+
+        [HttpGet]
+        [Route("parametritesti/{asiakasId:int}")]
+        public int[] ParametriTesti(int asiakasId)
+        {
+            int[] luvut = { 1, 3, 5, 7, 9 };
+            luvut[0] = asiakasId;
+
+            return luvut;
+        }
+
+        [HttpPost]
+        [Route("datasisään")]
+        public string DatanLukuSisään(SisääntulevaData data)
+        {
+            return $"Moi {data.Nimi}! Soitan sinulle pian numeroon {data.Puhelinnumero}.";
         }
     }
 }
---
+++ DotNet/WebApiDemo/WebApiDemo/Mallit/SisääntulevaData.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiDemo.Mallit
+{
+    public class SisääntulevaData
+    {
+        public string Nimi { get; set; }
+
+        public string Puhelinnumero { get; set; }
+    }
+}
