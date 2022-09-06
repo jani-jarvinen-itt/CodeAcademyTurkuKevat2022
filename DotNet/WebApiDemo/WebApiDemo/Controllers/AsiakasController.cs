7c49b76f0cba9785a04f2df60afef99ff8c2d829
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-09-06 15:19:57 +0300

    Lisätty API-kontrollerihin CRUD-toimintoja.

--- DotNet/WebApiDemo/WebApiDemo/Controllers/AsiakasController.cs
+++ DotNet/WebApiDemo/WebApiDemo/Controllers/AsiakasController.cs
@@ -8,12 +8,24 @@ namespace WebApiDemo.Controllers
     [ApiController]
     public class AsiakasController : ControllerBase
     {
+        [HttpGet]
+        [Route("")]
         public List<Customer> KaikkiAsikkaat()
         {
             NorthwindContext context = new();
             List<Customer> asiakkaat = context.Customers.ToList();
 
             return asiakkaat;
+        }
+
+        [HttpGet]
+        [Route("{asiakasId}")]
+        public Customer YksiAsiakas(string asiakasId)
+        {
+            NorthwindContext context = new();
+            Customer asiakas = context.Customers.Find(asiakasId)!;
+
+            return asiakas;
         }
     }
 }
